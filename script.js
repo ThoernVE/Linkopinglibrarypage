eba400048c098fb1a8c4e9727d308bc5a1501eff
ThoernVE <viktor.e.thorn@hotmail.com>
2024-11-01 11:16:49 +0100

    added a function to see the coordinates of the ISS

--- script.js
+++ script.js
@@ -6,18 +6,17 @@ function WeatherFunction() {
         .then((response) => response.json())  // Parse the JSON directly
         .then((weatherData) => {
             const weatherText = document.getElementById("weatherText"); // Assuming you have an element with this ID
-            translationSwitchCase(weatherData.weather[0].description)})
+            translationSwitchCase(weatherData.weather[0].description)
+        })
 
 }
 
-function translationSwitchCase(weatherENG)
-{
-    switch (weatherENG)
-    {
+function translationSwitchCase(weatherENG) {
+    switch (weatherENG) {
         case "thunderstorm with light rain":
             weatherText.innerHTML = "åskväder med lätt regn";
-            break;  
-            
+            break;
+
         case "thunderstorm with light rain":
             weatherText.innerHTML = "åskväder med lätt regn";
             break;
@@ -25,7 +24,7 @@ function translationSwitchCase(weatherENG)
         case "thunderstorm with rain":
             weatherText.innerHTML = "åskväder med regn";
             break;
-        
+
         case "thunderstorm with heavy rain":
             weatherText.innerHTML = "åskväder med kraftigt regn";
             break;
@@ -35,16 +34,16 @@ function translationSwitchCase(weatherENG)
             break;
 
         case "thunderstorm":
-             weatherText.innerHTML = "åskväder";
-             break;
+            weatherText.innerHTML = "åskväder";
+            break;
 
         case "heavy thunderstorm":
             weatherText.innerHTML = "kraftigt åskväder";
             break;
 
         case "ragged thunderstorm":
-             weatherText.innerHTML = "ojämnt åskväder";
-             break;
+            weatherText.innerHTML = "ojämnt åskväder";
+            break;
 
         case "thunderstorm with light drizzle":
             weatherText.innerHTML = "åskväder med lätt duggregn";
@@ -226,7 +225,7 @@ function translationSwitchCase(weatherENG)
             weatherText.innerHTML = "brutna moln";
             break;
 
-         case "overcast clouds":
+        case "overcast clouds":
             weatherText.innerHTML = "brutna moln";
             break;
 
@@ -257,166 +256,20 @@ function OpenMap() {
     }
 }
 
-/* case 1:
-        if (weatherENG == "thunderstorm with light rain") {
-        weatherText.innerHTML = "åskväder med lätt regn";
-    }
-    else if (weatherENG == "thunderstorm with light rain") {
-        weatherText.innerHTML = "åskväder med lätt regn";
-    }
-    else if (weatherENG == "thunderstorm with rain") {
-        weatherText.innerHTML = "åskväder med regn";
-    }
-    else if (weatherENG == "thunderstorm with heavy rain") {
-        weatherText.innerHTML = "åskväder med kraftigt regn";
-    }
-    else if (weatherENG == "light thunderstorm") {
-        weatherText.innerHTML = "lätt åskväder";
-    }
-    else if (weatherENG == "thunderstorm") {
-        weatherText.innerHTML = "åskväder";
-    }
-    else if (weatherENG == "heavy thunderstorm ") {
-        weatherText.innerHTML = "kraftigt åskväder";
-    }
-    else if (weatherENG == "ragged thunderstorm") {
-        weatherText.innerHTML = "ojämnt åskväder";
-    }
-    else if (weatherENG == "thunderstorm with light drizzle ") {
-        weatherText.innerHTML = "åskväder med lätt duggregn";
-    }
-    else if (weatherENG == "thunderstorm with drizzle") {
-        weatherText.innerHTML = "åskväder med duggregn";
-    }
-    else if (weatherENG == "thunderstorm with heavy drizzle") {
-        weatherText.innerHTML = "blixtoväder med kraftigt duggregn";
-    }
-    else if (weatherENG == "light intensity drizzle") {
-        weatherText.innerHTML = "lätt duggregn";
-    }
-    else if (weatherENG == "drizzle") {
-        weatherText.innerHTML = "duggregn";
-    }
-    else if (weatherENG == "heavy intensity drizzle ") {
-        weatherText.innerHTML = "kraftigt duggregn";
-    }
-    else if (weatherENG == "drizzle rain ") {
-        weatherText.innerHTML = "duggregn";
-    }
-    else if (weatherENG == "heavy intensity drizzle rain ") {
-        weatherText.innerHTML = "kraftigt duggregn";
-    }
-    else if (weatherENG == "shower rain and drizzle") {
-        weatherText.innerHTML = "regnskurar och duggregn";
-    }
-    else if (weatherENG == "heavy shower rain and drizzle") {
-        weatherText.innerHTML = "tunga regnskurar och duggregn";
-    }
-    else if (weatherENG == "shower drizzle") {
-        weatherText.innerHTML = "duggregnskurar";
-    }
-    else if (weatherENG == "light rain") {
-        weatherText.innerHTML = "lätt regn";
-    }
-    else if (weatherENG == "moderate rain") {
-        weatherText.innerHTML = "måttligt regn";
-    }
-    else if (weatherENG == "heavy intensity rain") {
-        weatherText.innerHTML = "kraftigt regn";
-    }
-    else if (weatherENG == "very heavy rain") {
-        weatherText.innerHTML = "väldigt kraftigt regn";
-    }
-    else if (weatherENG == "extreme rain") {
-        weatherText.innerHTML = "extremt regn";
-    }
-    else if (weatherENG == "freezing rain") {
-        weatherText.innerHTML = "underkylt regn";
-    }
-    else if (weatherENG == "light intensity shower rain ") {
-        weatherText.innerHTML = "lätta regnskurar";
-    }
-    else if (weatherENG == "shower rain") {
-        weatherText.innerHTML = "regnskurar";
-    }
-    else if (weatherENG == "heavy intensity shower rain ") {
-        weatherText.innerHTML = "tunga regnskurar";
-    }
-    else if (weatherENG == "ragged shower rain") {
-        weatherText.innerHTML = "ojämna regnskurar";
-    }
-    else if (weatherENG == "light snow") {
-        weatherText.innerHTML = "lätt snöfall";
-    }
-    else if (weatherENG == "snow") {
-        weatherText.innerHTML = "snöfall";
-    }
-    else if (weatherENG == "heavy snow") {
-        weatherText.innerHTML = "tungt snöfall";
-    }
-    else if (weatherENG == "sleet ") {
-        weatherText.innerHTML = "snöblandat regn";
-    }
-    else if (weatherENG == "light shower sleet ") {
-        weatherText.innerHTML = "lätta skurar med snöblandat regn";
-    }
-    else if (weatherENG == "shower sleet ") {
-        weatherText.innerHTML = "snöblandade regnskurar";
-    }
-    else if (weatherENG == "light rain and snow ") {
-        weatherText.innerHTML = "lätt regn och snö";
-    }
-    else if (weatherENG == "rain and snow") {
-        weatherText.innerHTML = "regn och snö";
-    }
-    else if (weatherENG == "light shower snow ") {
-        weatherText.innerHTML = "lätta snöskurar";
-    }
-    else if (weatherENG == "shower snow") {
-        weatherText.innerHTML = "snöskurar";
-    }
-    else if (weatherENG == "heavy shower snow") {
-        weatherText.innerHTML = "kraftiga snöskurar";
-    }
-    else if (weatherENG == "mist") {
-        weatherText.innerHTML = "dimma";
-    }
-    else if (weatherENG == "smoke") {
-        weatherText.innerHTML = "rök";
-    }
-    else if (weatherENG == "haze") {
-        weatherText.innerHTML = "dis";
-    }
-    else if (weatherENG == "fog") {
-        weatherText.innerHTML = "dimma";
-    }
-    else if (weatherENG == "sand") {
-        weatherText.innerHTML = "sand";
-    }
-    else if (weatherENG == "dust") {
-        weatherText.innerHTML = "damm";
-    }
-    else if (weatherENG == "volcanic ash") {
-        weatherText.innerHTML = "vulkanisk aska";
-    }
-    else if (weatherENG == "squalls") {
-        weatherText.innerHTML = "stormar";
-    }
-    else if (weatherENG == "tornado") {
-        weatherText.innerHTML = "tornado";
-    }
-    else if (weatherENG == "clear sky ") {
-        weatherText.innerHTML = "klara skyar";
-    }
-    else if (weatherENG == "few clouds: 11-25%") {
-        weatherText.innerHTML = "några moln: 11-25%";
-    }
-    else if (weatherENG == "scattered clouds: 25-50%") {
-        weatherText.innerHTML = "spridda moln: 25-50%";
-    }
-    else if (weatherENG == "broken clouds: 51-84%") {
-        weatherText.innerHTML = "brutna moln: 51-84%";
-    }
-    else if (weatherENG == "overcast clouds: 85-100% ") {
-        weatherText.innerHTML = "mulna moln: 85-100% ";
-    } */
+function fetchISSPosition() {
+    console.log("start");
+    fetch("http://api.open-notify.org/iss-now.json")
+        .then(response => response.json())
+        .then(data => {
+            const { latitude, longitude } = data.iss_position;
+            document.getElementById("iss-position").innerText = `Latitude: ${latitude}, Longitude: ${longitude}`;
+        })
+        .catch(error => {
+            document.getElementById("iss-position").innerText = "Failed to fetch ISS Position.";
+            console.error("Error fetching data:", error);
+        })
+    console.log("slut");
+}
+
+
+fetchISSPosition();
